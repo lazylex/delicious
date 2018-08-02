3e9d793511d187b6880a834752fabfad34755e40
Alexander Voloshin <lazylex@mail.ru>
2018-08-02 13:40:00 +0300

    add tables to migration

--- console/migrations/m180801_052555_create_db_structure.php
+++ console/migrations/m180801_052555_create_db_structure.php
@@ -14,37 +14,111 @@ class m180801_052555_create_db_structure extends Migration
     {
         $this->createTable("Category",
             [
-                'category_id' => $this->primaryKey(),
-                'parent_id' => $this->integer(),
-                'name' => $this->string(40)->notNull(),
-                'url' => $this->string(50)->notNull(),
+                'category_id' => $this->smallInteger()->unsigned()->notNull(),
+                'parent_id' => 'SMALLINT UNSIGNED',//может ли быть NULL?
+                'name' => $this->string(50)->notNull()->unique(),
+                'url' => $this->string()->notNull(),
+                'PRIMARY KEY(category_id)',
             ]);
 
         $this->createTable("Holidays",
             [
-                'holiday_id' => $this->primaryKey(),
+                'holiday_id' => $this->smallInteger()->unsigned()->notNull(),
                 'date' => $this->date()->notNull(),
-                'name' => $this->string(50)->notNull(),
+                'name' => $this->string(50)->notNull()->unique(),
+                'PRIMARY KEY(holiday_id)',
             ]
         );
 
         $this->createTable("Unit",
             [
-                'unit_id' => $this->primaryKey(),
+                'unit_id' => $this->smallInteger()->unsigned()->notNull(),
                 'name' => $this->string(30)->notNull(),
+                'PRIMARY KEY(unit_id)',
             ]);
 
-        $this->createTable("Ingridient",
+        $this->createTable("Ingredient",
             [
-                'ingridient_id' => $this->primaryKey(),
+                'ingredient_id' => $this->smallInteger()->unsigned()->notNull(),
                 'name' => $this->string(30)->notNull(),
                 'calories' => $this->integer(10),
-                'unit_id' => $this->integer()->notNull()
+                'unit_id' => $this->smallInteger()->unsigned()->notNull(),
+                'PRIMARY KEY(ingredient_id)',
+            ]);
+
+        $this->createTable("Ingredients",
+            [
+                'recipe_id' => $this->smallInteger()->unsigned()->notNull(),
+                'ingredient_id' => $this->smallInteger()->unsigned()->notNull(),
+                'count' => $this->integer()->notNull()->defaultValue(1),
+                'PRIMARY KEY(recipe_id, ingredient_id)',
+            ]);
+
+        $this->createTable("Recipe",
+            [
+                'recipe_id' => $this->smallInteger()->unsigned()->notNull(),
+                'name' => $this->string(),
+                'calories' => $this->integer(),
+                'time' => $this->time(),
+                'holiday_id' => $this->smallInteger()->unsigned(),
+                'author' => $this->smallInteger()->unsigned(),
+                'annotation' => $this->string(),
+                'article' => $this->text()->notNull(),
+                'category_id' => $this->smallInteger()->unsigned()->notNull(),
+                'PRIMARY KEY(recipe_id)',
             ]);
 
         //связываемые для внешних ключей поля должны быть ключами. Если это не так, нужно построить индекс
 
-        $this->addForeignKey('unit_id', 'Ingridient', 'unit_id', 'Unit', 'unit_id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey(
+            'fk_unit_id',
+            'Ingredient',
+            'unit_id',
+            'Unit',
+            'unit_id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk_ingredient_id',
+            'Ingredients',
+            'ingredient_id',
+            'Ingredient',
+            'ingredient_id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk_recipe_id',
+            'Ingredients',
+            'recipe_id',
+            'Recipe',
+            'recipe_id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk_holiday_id',
+            'Recipe',
+            'holiday_id',
+            'Holidays',
+            'holiday_id',
+            'CASCADE',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk_category_id',
+            'Recipe',
+            'category_id',
+            'Category',
+            'category_id',
+            'CASCADE',
+            'CASCADE'
+        );
     }
 
     /**
